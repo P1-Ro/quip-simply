8d606e21e2b191cefe5b61e6d2f43ab58b7cd637
Jozef Dochan <theglow666@gmail.com>
2020-10-24 22:04:43 +0200

    use websocket instead of long request

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
-from importer import do_import
+import os
+from flask_socketio import SocketIO
+
+from flask import Flask, render_template
+from importer import Importer
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "SECRET!!!")
+socketio = SocketIO(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/create_episode', methods=["POST"])
-def create_episode():
-    password = request.form['password']
-    do_import(password)
-    return "OK", 200
+@socketio.on("create_episode")
+def create_episode(data):
+    Importer().do_import(data["password"])
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    socketio.run(app)
--- importer.py
+++ importer.py
@@ -4,56 +4,80 @@ import random
 import time
 
 import unidecode
+from flask_socketio import emit
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
-NAME = "IMPORTER"
 
+class Importer:
+    EPISODE = "Best episode so far"
+    NAME = "IMPORTER"
+    step = 0
 
-def do_import(code):
-    chrome_bin = os.getenv('GOOGLE_CHROME_SHIM')
+    def do_import(self, code):
 
-    if chrome_bin:
-        opts = Options()
-        opts.binary_location = chrome_bin
-        driver = webdriver.Chrome(options=opts)
-    else:
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        chrome_bin = os.getenv("GOOGLE_CHROME_SHIM")
 
-    wait = WebDriverWait(driver, 10)
+        if chrome_bin:
+            opts = Options()
+            opts.binary_location = chrome_bin
+            driver = webdriver.Chrome(options=opts)
+        else:
+            driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.update_progress()
 
-    driver.get("https://jackbox.tv/")
-    time.sleep(2)
+        try:
+            wait = WebDriverWait(driver, 10)
 
-    driver.find_element_by_id("roomcode").send_keys(code)
-    driver.find_element_by_id("username").send_keys(NAME)
-    wait.until(EC.element_to_be_clickable((By.ID, "button-join")))
-    driver.find_element_by_id("button-join").click()
+            driver.get("https://jackbox.tv/")
+            time.sleep(2)
+            self.update_progress()
 
-    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ugc-action-new button, #ugc-new-button")))
-    driver.find_element_by_css_selector(".ugc-action-new button, #ugc-new-button").click()
+            driver.find_element_by_id("roomcode").send_keys(code)
+            driver.find_element_by_id("username").send_keys(self.NAME)
+            wait.until(EC.element_to_be_clickable((By.ID, "button-join")))
+            driver.find_element_by_id("button-join").click()
+            self.update_progress()
 
-    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ugc-action-title textarea,  #ugc-title-input")))
-    driver.find_element_by_css_selector(".ugc-action-title textarea,  #ugc-title-input").send_keys("Best episode so far")
-    driver.find_element_by_css_selector(".ugc-action-title button, #ugc-title-button").click()
+            wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ugc-action-new button, #ugc-new-button")))
+            driver.find_element_by_css_selector(".ugc-action-new button, #ugc-new-button").click()
+            self.update_progress()
 
-    file1 = codecs.open('lines.txt', 'r', encoding="utf-8")
-    lines = file1.readlines()
-    chosen_files = random.sample(lines, 64)
+            wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ugc-action-title textarea,  #ugc-title-input")))
+            driver.find_element_by_css_selector(".ugc-action-title textarea,  #ugc-title-input").send_keys(self.EPISODE)
+            driver.find_element_by_css_selector(".ugc-action-title button, #ugc-title-button").click()
+            self.update_progress()
 
-    for line in chosen_files:
-        fixed_line = unidecode.unidecode(line)
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ugc-action-add button, #ugc-add-button")))
-        driver.find_element_by_css_selector(".ugc-action-add textarea, #ugc-add-input").send_keys(fixed_line)
-        driver.find_element_by_css_selector(".ugc-action-add button, #ugc-add-button").click()
+            file1 = codecs.open("lines.txt", "r", encoding="utf-8")
+            lines = file1.readlines()
+            chosen_files = random.sample(lines, 64)
 
-    driver.find_element_by_css_selector(".ugc-action-done button, #ugc-save-button").click()
+            for line in chosen_files:
+                fixed_line = unidecode.unidecode(line)
+                wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ugc-action-add button, #ugc-add-button")))
+                driver.find_element_by_css_selector(".ugc-action-add textarea, #ugc-add-input").send_keys(fixed_line)
+                driver.find_element_by_css_selector(".ugc-action-add button, #ugc-add-button").click()
+                self.update_progress()
 
-    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ugc-action-play button, #ugc-play-button")))
-    driver.find_element_by_css_selector(".ugc-action-play button, #ugc-play-button").click()
+            driver.find_element_by_css_selector(".ugc-action-done button, #ugc-save-button").click()
+            self.update_progress()
 
-    driver.quit()
+            wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ugc-action-play button, #ugc-play-button")))
+            driver.find_element_by_css_selector(".ugc-action-play button, #ugc-play-button").click()
+            self.update_progress()
+        except TimeoutException:
+            emit("error", "Room with password {} not found".format(code))
+        except Exception as e:
+            print(e)
+            emit("error", "Unknown error occurred")
+
+        driver.quit()
+
+    def update_progress(self):
+        self.step += 1
+        emit("progress", {"step": self.step})
